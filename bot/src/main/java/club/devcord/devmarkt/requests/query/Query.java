384c4fa8e772593d2f1d155e437c879f24b0d482
Paul Kindler <pa179kin@gmail.com>
2024-09-07 18:15:41 +0200

    update Query + QueryResponse

--- bot/src/main/java/club/devcord/devmarkt/requests/query/Query.java
+++ bot/src/main/java/club/devcord/devmarkt/requests/query/Query.java
@@ -1,8 +1,11 @@
 package club.devcord.devmarkt.requests.query;
 
-import club.devcord.devmarkt.env.GlobalEnv;
+import club.devcord.devmarkt.DevmarktBot;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.URI;
@@ -16,20 +19,21 @@ import java.util.Collections;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public record Query(
     String query,
     Map<String, String> variables
 ) {
 
+  private static final Logger log = LoggerFactory.getLogger(Query.class);
+
   public static Query getQuery(String key, Map<String, String> variables) throws URISyntaxException, IOException {
-    var queryResourcePath = Query.class.getResource("graphql." + key);
+    var queryResourcePath = Query.class.getClassLoader().getResource("graphql/queries/" + key + ".graphql");
     if (queryResourcePath == null) {
       throw new IllegalArgumentException("No query found under query: [%s]".formatted(key));
     }
 
-    var graphqlQuery = Files.readString(
-        Paths.get(queryResourcePath.toURI())
-    );
+    var graphqlQuery = Files.readString(Paths.get(queryResourcePath.toURI()));
 
     return new Query(graphqlQuery, variables);
   }
@@ -41,14 +45,18 @@ public record Query(
   public CompletableFuture<QueryResponse> executeQuery(HttpClient client, ObjectMapper mapper) throws JsonProcessingException, URISyntaxException {
     var queryBody = mapper.writeValueAsString(this);
 
-    var request = HttpRequest.newBuilder()
-        .header("Authorization", "Self %s".formatted(GlobalEnv.envOrThrow("DEVMARKT_BOT_JWT_TOKEN")))
-        .uri(new URI(GlobalEnv.envOrThrow("BACKEND_URL") + "/graphql"))
+    var request = HttpRequest.newBuilder(URI.create(DevmarktBot.backendUrl + "/graphql"))
+        .header("Authorization", "Self %s".formatted(DevmarktBot.backendJwtToken))
+        .header("Content-Type", "application/json")
+        .header("Accept", "application/json")
         .POST(HttpRequest.BodyPublishers.ofString(queryBody))
         .build();
 
     return client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
         .thenApply(HttpResponse::body)
+        .whenComplete(((s, throwable) ->
+            log.debug(s))
+        )
         .thenApply(body -> {
           try {
             return mapper.readValue(body, QueryResponse.class);
--- bot/src/main/java/club/devcord/devmarkt/requests/query/QueryResponse.java
+++ bot/src/main/java/club/devcord/devmarkt/requests/query/QueryResponse.java
@@ -1,47 +1,28 @@
 package club.devcord.devmarkt.requests.query;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.jetbrains.annotations.Nullable;
-
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.net.http.HttpClient;
-import java.util.Collections;
 
 public record QueryResponse(
-    @Nullable String data,
-    @Nullable String extensions,
-    @Nullable String errors
+    JsonNode data,
+    String errors
 ) {
 
   public <T> T mapData(ObjectMapper mapper, Class<T> type) {
     try {
-      return mapper.readValue(this.data, type);
+      return mapper.treeToValue(data, type);
     } catch (JsonProcessingException e) {
       throw new RuntimeException(e);
     }
   }
 
   public boolean isSuccessful() {
-    return errors == null && (data != null && extensions != null);
+    return errors == null && (data != null);
   }
 
   public boolean isError() {
     return !isSuccessful();
-  }
-
-  record User(
-      String name,
-      String password
-  ) {
-  }
-
-  public static void main(String[] args) throws URISyntaxException, IOException {
-    var userResponse = new QueryResponse("user record in json", "kp was", null);
-
-    var client = HttpClient.newHttpClient();
-
-    var user = Query.getQuery("user").executeQueryMapped(client, User.class);
   }
 }
